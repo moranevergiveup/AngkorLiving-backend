cf25e4cda5f59ac281b4dd9a6dab98e0164eab35
V COMPUTER <marchsomroa8Gmail.com>
2026-04-04 01:51:19 +0700

    fix env

--- src/main/java/com/mora/angkorleving_backend/AngkorLevingBackendApplication.java
+++ src/main/java/com/mora/angkorleving_backend/AngkorLevingBackendApplication.java
@@ -2,8 +2,12 @@ package com.mora.angkorleving_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories("com.mora.angkorleving_backend.Repository") // repository package
+@EntityScan("com.mora.angkorleving_backend.entity")
 public class AngkorLevingBackendApplication {
 
     public static void main(String[] args) {
